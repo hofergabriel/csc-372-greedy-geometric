e1c20e3d5e1137b029cadb55ad6c2222e14cbbab
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-10-23 12:47:26 -0500

    ok

--- code/main.cpp
+++ code/main.cpp
@@ -42,34 +42,21 @@ int ccw(pair<int,int> a, pair<int,int> b, pair<int,int> c) {
     (b.first - a.first) * (c.second - b.second); 
 }
 
+/*********************************************************************
+Print Stack
+*********************************************************************/
 void printStack(vector<pair<int,int>> &s){
-  for(int i=0;i<s.size();i++){
+  for(int i=0;i<s.size();i++)
     cout<<"\t"<<s[i].first<<' '<<s[i].second<<endl;
-  }
   cout<<endl;
 }
 
-
 /*********************************************************************
-https://en.wikipedia.org/wiki/Graham_scan#Pseudocode
-
-let points be the list of points
-let stack = empty_stack()
-
-find the lowest y-coordinate and leftmost point, called P0
-sort points by polar angle with P0, if several points have the same polar angle then only keep the farthest
-
-for point in points:
-    # pop the last point from the stack if we turn clockwise to reach this point
-    while count stack > 1 and ccw(next_to_top(stack), top(stack), point) <= 0:
-        pop stack
-    push point to stack
-end
+Graham's Scan
 *********************************************************************/
 vector<pair<int,int>> graham(vector<pair<int,int>> &points){
   vector<pair<int,int>> stack;
   for(int i=0;i<points.size();i++){
-    cout<<"current point: "<<points[i].first<<' '<<points[i].second<<endl;
     while(stack.size()>1 && ccw(stack[stack.size()-2], stack.back(), points[i]) <= 0)
       stack.pop_back();
     stack.push_back(points[i]);
@@ -78,19 +65,15 @@ vector<pair<int,int>> graham(vector<pair<int,int>> &points){
   return stack;
 }
 
+/*********************************************************************
+Sort points by polar angle
+*********************************************************************/
 void polarSort(vector<pair<int,int>> &v){
-  sort(v.begin(),v.end());
-  cout<<"first point: "<<v[0].first<<' '<<v[0].second<<endl;
+  sort(v.begin(),v.end()); // find v[0]
   set<pair<float,pair<int,int>>> s;
-  for(int i=1;i<v.size();i++)
-    s.insert({atan2(v[i].second-v[0].second,v[i].first-v[0].first),v[i]});
-  s.insert({-10000,v[0]});
+  for(int i=1;i<v.size();i++) s.insert({atan2(v[i].second-v[0].second,v[i].first-v[0].first),v[i]});
+  s.insert({-100,v[0]});
   v.clear();
-
-  for(auto e:s)
-    cout<<"theta: "<<e.first<<" "<<e.second.first<<"  "<<e.second.second<<endl;
-  cout<<endl;
-
   for(auto e:s) v.push_back(e.second);
   reverse(v.begin()+1,v.end());
 }
@@ -101,13 +84,11 @@ Main
 *********************************************************************/
 int main(int argc, char ** argv){
 
-
   int threshold=atoi(argv[2]);
   vector<pair<int,int>> points=getPoints(argv);
 
   polarSort(points);
   printStack(points);
-
 
   vector<pair<int,int>> cvhull = graham(points);
   cout<<"cvhull size: "<<cvhull.size()<<endl;
